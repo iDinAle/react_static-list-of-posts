4f3a4ad171217ec3b3c5548a846cf939a7fb7600
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-04 21:54:16 +0200

    Updates

---
+++ precache-manifest.fa9781c940836dbb05519c7f1c0b840b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22dec16cc7d365cdf5e861fcd1e3d99",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "0f114b127aca0412d81b",
+    "url": "/react_static-list-of-posts/static/css/main.f928b0ac.chunk.css"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-posts/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "0f114b127aca0412d81b",
+    "url": "/react_static-list-of-posts/static/js/main.cd830353.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
